74537c6e9c16ae3d9740cc0c2e5511a4bf9035e5
Boluwatife Oladejo <boluwatifeoladejo@gmail.com>
2022-12-22 17:28:23 +0000

    updated body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import "./Body.css";
 
 function Body() {
   return (
     <div className='bodyd'>
        <img src="../images/wishlist.png" alt="My Image"/>
+       <h3>Nothing on Wishlist</h3>
+       <p>Proceed to store and start shopping</p>
+       <button>Go to Shop</button>
     </div>
   )
 }
